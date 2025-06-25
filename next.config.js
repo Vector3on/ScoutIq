7a1f281d404ceb7141dd0b42320d7cd8b4a6533b
Vector3on <ruthvik.vector@gmail.com>
2025-06-26 05:06:17 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // This line tells Next.js to export the site as static files.
+  output: 'export',
 };
 
 module.exports = nextConfig;
